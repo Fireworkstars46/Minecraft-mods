adb8fe0c4edc8420d748a1d19d2885251d588b32
Fireworkstars46 <garrettcarey46@aol.com>
2026-08-24 01:15:48 -0400

    Add v2.1 application class

---
+++ v21src/app/src/main/java/com/garrett/appvolumestepper/App.java
@@ -0,0 +1,32 @@
+package com.garrett.appvolumestepper;
+
+import android.app.Application;
+import android.content.Context;
+
+import org.lsposed.hiddenapibypass.HiddenApiBypass;
+
+public class App extends Application {
+    private static App instance;
+    private SamsungAppVolumeManager manager;
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        HiddenApiBypass.addHiddenApiExemptions("");
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        instance = this;
+        manager = new SamsungAppVolumeManager(this);
+    }
+
+    public static App get() {
+        return instance;
+    }
+
+    public SamsungAppVolumeManager manager() {
+        return manager;
+    }
+}
